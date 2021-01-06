5679ccbddf84da33abb784f76902a185b229fcd1
Mark Ganhao <mark.ganhao@gmail.com>
2021-01-06 13:03:24 +0000

    init

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "4z9l1gm9",    // found in sanityfolder/sanity.json || manage.sanity.io on your project page
+  projectId: "94yasw8t",    // found in sanityfolder/sanity.json || manage.sanity.io on your project page
   dataset: "production"
 })
